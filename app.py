225b544b4eb7029560caddd449eca0a657407c4e
John Rofrano <johnnyroy@johnrofrano.com>
2019-04-26 15:50:18 -0400

    added PORT from environment

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
+"""
+Hit Counter Service
+
+Start the Hit Counter Service and initializes logging
+"""
+import os
 from hitcounter import app, routes
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+# Pull options from environment
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+PORT = os.getenv('PORT', '5000')
+
+######################################################################
+#   M A I N
+######################################################################
+if __name__ == "__main__":
+    print(''.center(70, '*'))
+    print('  H I T   C O U N T E R   S E R V I C E '.center(70, '*'))
+    print(''.center(70, '*'))
+    app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
